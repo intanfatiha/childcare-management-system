0c0112b4c71926fbdb9dc59ed420c4e098fa620d
Intan Nur Fatiha <intanfatiha292@gmail.com>
2025-02-25 16:39:39 +0800

    commit with home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in Intan!,,') }}
+                    {{ __('You are logged in User12345!,,') }}
                 </div>
             </div>
         </div>
